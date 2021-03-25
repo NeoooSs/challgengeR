d938afc5878b02f82fffa7d62c0260582c505f7f
NeoooS <t.dossantos45190@gmail.com>
2021-03-25 16:28:35 +0100

    Ajout de la fonction erreur kppv

--- main.R
+++ main.R
@@ -26,18 +26,24 @@ mrPropre<-function(corpus){
   corpus<-tm_map(corpus,stemDocument,language='en')
 }
 
+erreur_kppv <- function(k,data){
+  predictions <- vapply(seq_len(nrow)) function(i){
+    classerKPPV(data[i, -ncol(data)], k, data[-i,])
+  }, numeric(1L)
+  sum(predictions != data[, ncol(data)])
+}
+
 
 train<-VCorpus(DirSource("training",recursive=TRUE))
 trainN<-mrPropre(train)
-trainN
 mat<-DocumentTermMatrix(trainN)
 
 vocab<-findFreqTerms(mat,lowfreq=200)
-vec<-sapply(1:50,function(i)length(findFreqTerms(mat,lowfreq=i)))
-mat20<-DocumentTermMatrix(trainN,control=list(dictionary=vocab))
-M<-as.matrix(mat20)
-classes<-c(rep(0,5),rep(1,5),rep(2,5))
+mat200<-DocumentTermMatrix(trainN,control=list(dictionary=vocab))
+M<-as.matrix(mat200)
+classes<-c(rep(0,150),rep(1,150),rep(2,150),rep(3,150),rep(4,150),rep(5,150),rep(6,150))
 M<-cbind(M,classes)
+M<-erreur_kppv(1050,M)
 
 #classer<-function(fic){
   #corpus<-Vcorpus(URISource(fic))
